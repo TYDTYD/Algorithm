85608b918b44f82301d174cf043d288ceb52d7a5
TYDTYD <tmdals5587@naver.com>
2022-02-19 23:35:49 +0900

    second commit

--- 벨만포드 알고리즘.py
+++ 벨만포드 알고리즘.py
@@ -5,7 +5,7 @@ INF=int(1e9)
 start=1
 n,m=map(int,input().split())
 
-distance=[INF]*(n+1)
+distance=[INF]*(n+1) # 모든 간선 무한대로 초기화
 graph=[]
 
 for i in range(m):
@@ -17,15 +17,15 @@ def bellmanford(graph,start):
     for i in range(n):
         for j in range(m):
             if distance[graph[j][1]]>distance[graph[j][0]]+graph[j][2] and distance[graph[j][0]]!=INF:
-                distance[graph[j][1]]=distance[graph[j][0]]+graph[j][2]
+                distance[graph[j][1]]=distance[graph[j][0]]+graph[j][2] # 최단 경로 갱신
                 if i==n-1: # n을 넘어가면 음의 사이클이므로
                     return True
     return False
 
 cycle=bellmanford(graph,start)
 
-if cycle==False:
-    for i in range(2,n+1):# 시작하는 값 제외
+if cycle==False: # 음의 사이클이 없다면
+    for i in range(2,n+1): # 시작하는 값 제외
         if distance[i]==INF:
             print(-1)
         else:
