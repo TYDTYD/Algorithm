04a60f48cc362059c26faa28cb1836651bf2b66a
TYDTYD <tmdals5587@naver.com>
2022-05-18 00:17:50 +0900

    second commit

--- 에라토스테네스의 체.py
+++ 에라토스테네스의 체.py
@@ -7,16 +7,14 @@ n=int(input())
 a=list(map(int,input().split()))
 
 array=[True for i in range(max(a)+1)]
+number=[]
 
 for i in range(2,int(math.sqrt(max(a)))+1):
     if array[i]==True:
+        number.append(i)
         j=2
         while i*j<=max(a):
             array[i*j]=False
             j+=1
 
-for i in a:
-    if array[i] and i!=1 and i!=0:
-        count+=1
-
-print(count)
+print(number)
